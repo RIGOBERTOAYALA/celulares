22b5f591ee51285608179afb4ac938129dc14988
unknown <Rigoberto318722@gmail.com>
2019-05-06 20:47:37 -0500

    Agregar formulario fabricante

---
+++ Celulares/Celulares/Viws/FrmFabricantes.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Celulares.Viws
+{
+    partial class FrmFabricantes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlFiltro = new System.Windows.Forms.GroupBox();
+            this.pnlFabricantes = new System.Windows.Forms.GroupBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblPis = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.cboPais = new System.Windows.Forms.ComboBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.grdDatos = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.pnlFiltro.SuspendLayout();
+            this.pnlFabricantes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlFiltro
+            // 
+            this.pnlFiltro.Controls.Add(this.btnLimpiar);
+            this.pnlFiltro.Controls.Add(this.cboPais);
+            this.pnlFiltro.Controls.Add(this.btnBuscar);
+            this.pnlFiltro.Controls.Add(this.txtNombre);
+            this.pnlFiltro.Controls.Add(this.lblPis);
+            this.pnlFiltro.Controls.Add(this.lblNombre);
+            this.pnlFiltro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pnlFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlFiltro.Location = new System.Drawing.Point(13, 13);
+            this.pnlFiltro.Name = "pnlFiltro";
+            this.pnlFiltro.Size = new System.Drawing.Size(641, 137);
+            this.pnlFiltro.TabIndex = 0;
+            this.pnlFiltro.TabStop = false;
+            this.pnlFiltro.Text = "Filtros";
+            this.pnlFiltro.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // pnlFabricantes
+            // 
+            this.pnlFabricantes.Controls.Add(this.grdDatos);
+            this.pnlFabricantes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlFabricantes.Location = new System.Drawing.Point(13, 175);
+            this.pnlFabricantes.Name = "pnlFabricantes";
+            this.pnlFabricantes.Size = new System.Drawing.Size(641, 228);
+            this.pnlFabricantes.TabIndex = 1;
+            this.pnlFabricantes.TabStop = false;
+            this.pnlFabricantes.Text = "Fabricantes";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(7, 30);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(58, 45);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre\r\n\r\n:";
+            // 
+            // lblPis
+            // 
+            this.lblPis.AutoSize = true;
+            this.lblPis.Location = new System.Drawing.Point(10, 61);
+            this.lblPis.Name = "lblPis";
+            this.lblPis.Size = new System.Drawing.Size(39, 15);
+            this.lblPis.TabIndex = 1;
+            this.lblPis.Text = "Pais:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(91, 30);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 21);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(331, 47);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 30);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // cboPais
+            // 
+            this.cboPais.FormattingEnabled = true;
+            this.cboPais.Location = new System.Drawing.Point(91, 57);
+            this.cboPais.Name = "cboPais";
+            this.cboPais.Size = new System.Drawing.Size(121, 23);
+            this.cboPais.TabIndex = 5;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(466, 46);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(100, 30);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // grdDatos
+            // 
+            this.grdDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDatos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdDatos.Location = new System.Drawing.Point(3, 16);
+            this.grdDatos.Name = "grdDatos";
+            this.grdDatos.Size = new System.Drawing.Size(635, 209);
+            this.grdDatos.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Location = new System.Drawing.Point(16, 425);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(100, 30);
+            this.btnRegresar.TabIndex = 2;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            // 
+            // FrmFabricantes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 523);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.pnlFabricantes);
+            this.Controls.Add(this.pnlFiltro);
+            this.Name = "FrmFabricantes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestion de Fabricantes";
+            this.pnlFiltro.ResumeLayout(false);
+            this.pnlFiltro.PerformLayout();
+            this.pnlFabricantes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdDatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox pnlFiltro;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.ComboBox cboPais;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblPis;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.GroupBox pnlFabricantes;
+        private System.Windows.Forms.DataGridView grdDatos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRegresar;
+    }
+}
---
+++ Celulares/Celulares/Viws/FrmFabricantes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Celulares.Viws
+{
+    public partial class FrmFabricantes : Form
+    {
+        public FrmFabricantes()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
